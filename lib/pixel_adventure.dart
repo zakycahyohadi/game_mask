0b972a2de81a07c23809f087f873667620fa883f
Ageng HadiWicaksono <agenghw27@gmail.com>
2026-01-31 14:19:40 +0700

    delete

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
 import 'package:flutter/painting.dart';
 import 'package:pixel_adventure/components/jump_button.dart';
 import 'package:pixel_adventure/components/player.dart';
@@ -19,7 +18,6 @@ class PixelAdventure extends FlameGame
   Color backgroundColor() => const Color(0xFF211F30);
   late CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
-  late JoystickComponent joystick;
   bool showControls = false;
   bool playSounds = true;
   double soundVolume = 1.0;
@@ -34,56 +32,10 @@ class PixelAdventure extends FlameGame
     _loadLevel();
 
     if (showControls) {
-      addJoystick();
       add(JumpButton());
     }
 
     return super.onLoad();
-  }
-
-  @override
-  void update(double dt) {
-    if (showControls) {
-      updateJoystick();
-    }
-    super.update(dt);
-  }
-
-  void addJoystick() {
-    joystick = JoystickComponent(
-      priority: 10,
-      knob: SpriteComponent(
-        sprite: Sprite(
-          images.fromCache('HUD/Knob.png'),
-        ),
-      ),
-      background: SpriteComponent(
-        sprite: Sprite(
-          images.fromCache('HUD/Joystick.png'),
-        ),
-      ),
-      margin: const EdgeInsets.only(left: 32, bottom: 32),
-    );
-
-    add(joystick);
-  }
-
-  void updateJoystick() {
-    switch (joystick.direction) {
-      case JoystickDirection.left:
-      case JoystickDirection.upLeft:
-      case JoystickDirection.downLeft:
-        player.horizontalMovement = -1;
-        break;
-      case JoystickDirection.right:
-      case JoystickDirection.upRight:
-      case JoystickDirection.downRight:
-        player.horizontalMovement = 1;
-        break;
-      default:
-        player.horizontalMovement = 0;
-        break;
-    }
   }
 
   void loadNextLevel() {
